11e142c8d5c08313b6e17eddf4fb8dd27bcab639
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-02-03 15:31:38 -0500

    Added API request for random video, setup basic UI for API request to Ticketmaster, note all requests are commented right now

--- honey-bee-angular/src/app/app.component.ts
+++ honey-bee-angular/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'honey-bee-angular';
+  title = "honey-bee-angular";
+  imgUrl = String();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // let headers = new HttpHeaders().set(
+    //   "Authorization",
+    //   "563492ad6f91700001000001960d404e870a4a568d7abc872cdfbab5"
+    // );
+    // let obs = this.http.get(
+    //   "https://api.pexels.com/videos/search?query=party&per_page=15&page=1",
+    //   { headers }
+    // );
+    // obs.subscribe(response => {
+    //   console.log(response["videos"][0]["video_files"][0]["link"]);
+    //   this.imgUrl = response["videos"][0]["video_files"][0]["link"];
+    // });
+  }
 }
